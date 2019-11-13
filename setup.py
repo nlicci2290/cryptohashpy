c25cad668ed131a55d63560ae448285f3568ad45
Nick Liccione <nlicci2290@gmail.com>
2019-11-12 21:22:21 -0500

    New project structure and added .gitignore

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, Extension
 
-ext_modules = [ Extension('cryptohashpy', ['cryptohash/cryptohash.c', 'cryptohash/md5.c']) ]
+source_dir = "cryptohash/source/"
+
+ext_modules = [ 
+    Extension('cryptohashpy', 
+              [ source_dir + 'main_cryptohash.c', source_dir +  "md5/md5.c",  source_dir +  "md5/md5_py.c"],  
+              include_dirs = [ 'cryptohash/include' ]) 
+]
 
 setup(name='cryptohashpy', version='1.0', ext_modules = ext_modules)
